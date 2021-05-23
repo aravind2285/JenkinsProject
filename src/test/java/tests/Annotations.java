903e5b19e87cf2adb678fa91a069ee4337601aac
aravind2285 <aravind8522@gmail.com>
2021-05-23 14:28:41 +0530

    Initial commit of all files to the repository

---
+++ src/test/java/tests/Annotations.java
@@ -0,0 +1,11 @@
+package tests;
+
+import org.testng.annotations.Test;
+
+public class Annotations {
+	@Test
+	public void firstMethod() {
+		System.out.println(".............My first method for JENKINS............");
+
+	}
+}
